2f31a06442e6581d0d240fba14849c42dd2338cf
Paula Cristina Martins <paula172326@gmail.com>
2024-06-25 10:10:06 -0300

    feat(SignIn): criando página de login no sistema.

---
+++ src/pages/SignIn/index.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+
+import { useAuth } from "../../hooks/auth";
+
+import { Container, Form } from "./styles";
+
+import Logo from "../../assets/logo.svg";
+import { BrandImage } from "../../components/BrandImage";
+import { Input } from "../../components/Input";
+import { Link } from "react-router-dom";
+import { Button } from "../../components/Button";
+
+export function SignIn() {
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+	const { signIn } = useAuth();
+
+	function handleSignIn() {
+		signIn({ email, password });
+	}
+
+	return (
+		<Container>
+			<BrandImage size={2.6} />
+			<Form>
+				<h1>Faça login</h1>
+
+				<Input
+					title={"Email"}
+					type={"text"}
+					placeholder={"exemplo@exemplo.com.br"}
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+				/>
+
+				<Input
+					title={"Senha"}
+					type={"password"}
+					placeholder={"- - - - - - -"}
+					value={password}
+					onChange={(e) => setPassword(e.target.value)}
+				/>
+
+				<Button name={"Entrar"} onClick={handleSignIn} />
+
+				<Link to={"/register"}>Criar uma conta</Link>
+			</Form>
+		</Container>
+	);
+}
