a42133603033eddef4ea958b4f59496094537c35
Paula Cristina Martins <paula172326@gmail.com>
2024-06-25 22:42:21 -0300

    refact(Button): inclusão de ícone no botão.

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,8 +1,13 @@
 import { Container } from "./styles";
 
-export function Button({ name, onClick, ...rest }) {
+export function Button({ name, onClick, icon: Icon, ...rest }) {
 	return (
 		<Container type={"button"} onClick={onClick}>
+			{Icon && (
+				<span>
+					<Icon size={20} />
+				</span>
+			)}
 			{name}
 		</Container>
 	);
--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,8 +1,11 @@
 import styled from "styled-components";
 
 export const Container = styled.button`
-	width: 100%;
-	padding: 0.9rem;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+
+	padding: ${({ Icon }) => "1rem 2rem" || "0.9rem"};
 	font-size: 0.9rem;
 
 	border: 0;
@@ -10,4 +13,12 @@ export const Container = styled.button`
 
 	background: ${({ theme }) => theme.COLORS.RED_800};
 	color: ${({ theme }) => theme.COLORS.LIGHT_100};
+
+	cursor: pointer;
+
+	span {
+		display: flex;
+		align-items: center;
+		padding-right: 0.5rem;
+	}
 `;
