c15615eab99395197817c2a13d8e54e41a19536c
Paula Cristina Martins <paula172326@gmail.com>
2024-06-28 20:08:45 -0300

    feat(Consulta): implementando componente de consulta de pratos / ingredientes.

---
+++ src/components/ConsultFoods/index.jsx
@@ -0,0 +1,20 @@
+import { FiSearch } from "react-icons/fi";
+import { BrandImage } from "../BrandImage";
+import { Container } from "./styles";
+
+export function ConsultFoods({ description, setDescription }) {
+	return (
+		<Container>
+			<span className="input-container">
+				<FiSearch size={20} className="search-icon" />
+				<input
+					type={"text"}
+					value={description}
+					onChange={(e) => setDescription(e.target.value)}
+					placeholder={"Busque por pratos ou ingredientes"}
+					autoComplete={"description"}
+				/>
+			</span>
+		</Container>
+	);
+}
---
+++ src/components/ConsultFoods/styles.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import { DEVICE_BREAKPOINTS } from "../../styles/deviceBreakpoints";
+
+export const Container = styled.div`
+	flex-grow: 1;
+	width: 100%;
+
+	.input-container {
+		position: relative;
+		display: flex;
+		align-items: center;
+	}
+
+	input {
+		padding: 0.9rem;
+		padding-left: 3rem;
+		border-radius: 0.3rem;
+		border: 0;
+		background: ${({ theme }) => theme.COLORS.DARK_200};
+		color: ${({ theme }) => theme.COLORS.LIGHT_100};
+		width: 100%;
+
+		&::placeholder {
+			color: ${({ theme }) => theme.COLORS.LIGHT_500};
+		}
+	}
+
+	.search-icon {
+		position: absolute;
+		left: 1rem;
+		pointer-events: none;
+		color: ${({ theme }) => theme.COLORS.LIGHT_500};
+	}
+`;
