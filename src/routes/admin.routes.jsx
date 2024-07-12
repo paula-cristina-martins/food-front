e157601be62608f39a43da33d8e861f1c4ce44a2
Paula Cristina Martins <paula172326@gmail.com>
2024-07-11 22:08:06 -0300

    feat(Routes): adicionando rotas de criação e atualização dos pratos.

--- src/routes/admin.routes.jsx
+++ src/routes/admin.routes.jsx
@@ -2,12 +2,16 @@ import { Route, Routes } from "react-router-dom";
 import { NotFound } from "../pages/NotFound";
 import { Home } from "../pages/Home";
 import { ShowFood } from "../pages/Food";
+import { UpdateFood } from "../pages/Food/Update";
+import { CreateFood } from "../pages/Food/Create";
 
 export function AdminRoutes() {
 	return (
 		<Routes>
 			<Route path="/" element={<Home />} />
 			<Route path="/show-food/:id" element={<ShowFood />} />
+			<Route path="/update-food/:id" element={<UpdateFood />} />
+			<Route path="/create-food" element={<CreateFood />} />
 			<Route path="*" exact={true} element={<NotFound />} />
 		</Routes>
 	);
